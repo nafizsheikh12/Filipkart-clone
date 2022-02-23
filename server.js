13bf124f5146ebb80c8df20a0bf3447af723621a
nafizsheikh12 <nafizsheikh394@gmail.com>
2022-02-24 03:35:32 +0600

    make it better

--- server.js
+++ server.js
@@ -27,12 +27,9 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
-//build
-app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-});
+
+
 
 //middleware
 app.use(bodyParser.json({extended:true}))
